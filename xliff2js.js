669254095a06500649335c4129d510c560689692
Adriano Raiano <adriano@raiano.ch>
2018-11-16 13:07:30 +0100

    edge case

--- xliff2js.js
+++ xliff2js.js
@@ -21,39 +21,41 @@ function xliffToJs(str, cb) {
 
   const xliffRoot = xmlObj.elements[0];
 
-  const srcLang = xliffRoot.attributes['srcLang'];
-  const trgLang = xliffRoot.attributes['trgLang'];
+  if (xliffRoot.attributes) {
+    const srcLang = xliffRoot.attributes['srcLang'];
+    const trgLang = xliffRoot.attributes['trgLang'];
 
-  result.sourceLanguage = srcLang;
-  result.targetLanguage = trgLang;
+    result.sourceLanguage = srcLang;
+    result.targetLanguage = trgLang;
 
-  result.resources = xliffRoot.elements.reduce((resources, file) => {
-    const namespace = file.attributes.id;
+    result.resources = xliffRoot.elements.reduce((resources, file) => {
+      const namespace = file.attributes.id;
 
-    // namespace
-    resources[namespace] = file.elements.reduce((file, unit) => {
-      const key = unit.attributes.id;
+      // namespace
+      resources[namespace] = file.elements.reduce((file, unit) => {
+        const key = unit.attributes.id;
 
-      // source, target, note
-      file[key] = unit.elements.reduce((unit, segment) => {
-        segment.elements.forEach((element) => {
-          switch (element.name) {
-            case 'source':
-            case 'target':
-            case 'note':
-              unit[element.name] = extractValue(element.elements, ElementTypes2);
-              break;
-          }
-        });
+        // source, target, note
+        file[key] = unit.elements.reduce((unit, segment) => {
+          segment.elements.forEach((element) => {
+            switch (element.name) {
+              case 'source':
+              case 'target':
+              case 'note':
+                unit[element.name] = extractValue(element.elements, ElementTypes2);
+                break;
+            }
+          });
 
-        return unit;
-      }, { source: '', target: '' });
+          return unit;
+        }, { source: '', target: '' });
 
-      return file;
+        return file;
+      }, {});
+
+      return resources;
     }, {});
-
-    return resources;
-  }, {});
+  }
 
   if (cb) return cb(null, result);
   return result;
