5f349987d75388244534d553239344b0c729007d
Adriano Raiano <adriano@raiano.ch>
2022-01-02 12:41:06 +0100

    encapsulate fixNL function

---
+++ test/fixtures/fixNewLines.js
@@ -0,0 +1,1 @@
+module.exports = (txt) => txt.replace(/\r\n/g, '\n').replace(/\n$/, '')
--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -1,15 +1,16 @@
 const fs = require('fs')
 const path = require('path')
+const fixNewLines = require('./fixNewLines')
 
 module.exports = {
   example: {
     js: require('./example.json'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, ''),
-    xliff_output_notes: fs.readFileSync(path.join(__dirname, 'example_output_note.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, ''),
-    xliff_compact: fs.readFileSync(path.join(__dirname, 'example_compact.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, ''),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example12.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, ''),
-    xliff12_output_notes: fs.readFileSync(path.join(__dirname, 'example12_output_note.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, ''),
-    xliff12_compact: fs.readFileSync(path.join(__dirname, 'example_compact12.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, ''),
+    xliff: fixNewLines(fs.readFileSync(path.join(__dirname, 'example.xliff')).toString()),
+    xliff_output_notes: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_output_note.xliff')).toString()),
+    xliff_compact: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_compact.xliff')).toString()),
+    xliff12: fixNewLines(fs.readFileSync(path.join(__dirname, 'example12.xliff')).toString()),
+    xliff12_output_notes: fixNewLines(fs.readFileSync(path.join(__dirname, 'example12_output_note.xliff')).toString()),
+    xliff12_compact: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_compact12.xliff')).toString()),
     js_source: require('./example_source.json'),
     js_target: require('./example_target.json'),
     js_input_notes: require('./example_input_note.json'),
@@ -17,50 +18,50 @@ module.exports = {
   },
   example_source_attr: {
     // The JS should be the same as example.js
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_source_attr12.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, '')
+    xliff12: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_source_attr12.xliff')).toString())
   },
   example_multi: {
     js: require('./example_multi.json'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_multi.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, ''),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_multi12.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, ''),
+    xliff: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_multi.xliff')).toString()),
+    xliff12: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_multi12.xliff')).toString()),
     js_source: require('./example_multi_source.json'),
     js_target: require('./example_multi_target.json')
   },
   example_note: {
     js: require('./example_note.json'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_note.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, ''),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_note12.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, '')
+    xliff: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_note.xliff')).toString()),
+    xliff12: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_note12.xliff')).toString())
   },
   example_additional_attributes: {
     js: require('./example_additional_attributes'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_additional_attributes.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, ''),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_additional_attributes12.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, '')
+    xliff: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_additional_attributes.xliff')).toString()),
+    xliff12: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_additional_attributes12.xliff')).toString())
   },
   example_angular: {
     js: require('./example_angular'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_angular.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, ''),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_angular12.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, '')
+    xliff: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_angular.xliff')).toString()),
+    xliff12: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_angular12.xliff')).toString())
   },
   example_google: {
     js: require('./example_google_action'),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_google_action.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, '')
+    xliff12: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_google_action.xliff')).toString())
   },
   example_groups: {
     js: require('./example_groups'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_groups.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, ''),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_groups12.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, '')
+    xliff: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_groups.xliff')).toString()),
+    xliff12: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_groups12.xliff')).toString())
   },
   example_cdata: {
     js: require('./example_cdata'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_cdata.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, ''),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_cdata12.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, '')
+    xliff: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_cdata.xliff')).toString()),
+    xliff12: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_cdata12.xliff')).toString())
   },
   example_comments: {
     js: require('./example_comments'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_comments.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, '')
+    xliff: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_comments.xliff')).toString())
   },
   example_ignorable: {
     js: require('./example_ignorable'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_ignorable.xliff')).toString().replace(/\r\n/g, '\n').replace(/\n$/, '')
+    xliff: fixNewLines(fs.readFileSync(path.join(__dirname, 'example_ignorable.xliff')).toString())
   }
 }
