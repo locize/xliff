72a0fa732b716d2cbd0803b41d3418eacb86ab4b
Adriano Raiano <adriano@raiano.ch>
2022-01-02 12:14:42 +0100

    adapt newlines for windows in tests

--- test/fixtures/inline-elements/index.js
+++ test/fixtures/inline-elements/index.js
@@ -4,61 +4,61 @@ const path = require('path')
 module.exports = {
   example_standalone: {
     js: require('./example_standalone.json'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_standalone.xliff')).toString().replace(/\n$/, ''),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_standalone12.xliff')).toString().replace(/\n$/, '')
+    xliff: fs.readFileSync(path.join(__dirname, 'example_standalone.xliff')).toString().replace(/\n$/, '').replace(/\r$/, ''),
+    xliff12: fs.readFileSync(path.join(__dirname, 'example_standalone12.xliff')).toString().replace(/\n$/, '').replace(/\r$/, '')
   },
   example_standalone_only: {
     js: require('./example_standalone_only.json'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_standalone_only.xliff')).toString().replace(/\n$/, ''),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_standalone_only12.xliff')).toString().replace(/\n$/, '')
+    xliff: fs.readFileSync(path.join(__dirname, 'example_standalone_only.xliff')).toString().replace(/\n$/, '').replace(/\r$/, ''),
+    xliff12: fs.readFileSync(path.join(__dirname, 'example_standalone_only12.xliff')).toString().replace(/\n$/, '').replace(/\r$/, '')
   },
   example_genericSpan: {
     js: require('./example_generic-span.json'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_generic-span.xliff')).toString().replace(/\n$/, ''),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_generic-span12.xliff')).toString().replace(/\n$/, ''),
+    xliff: fs.readFileSync(path.join(__dirname, 'example_generic-span.xliff')).toString().replace(/\n$/, '').replace(/\r$/, ''),
+    xliff12: fs.readFileSync(path.join(__dirname, 'example_generic-span12.xliff')).toString().replace(/\n$/, '').replace(/\r$/, ''),
     js_source: require('./example_generic-span_source.json'),
     js_target: require('./example_generic-span_target.json')
   },
   example_genericSpanStartEnd: {
     js: require('./example_generic-span-start-end.json'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_generic-span-start-end.xliff')).toString().replace(/\n$/, ''),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_generic-span-start-end12.xliff')).toString().replace(/\n$/, '')
+    xliff: fs.readFileSync(path.join(__dirname, 'example_generic-span-start-end.xliff')).toString().replace(/\n$/, '').replace(/\r$/, ''),
+    xliff12: fs.readFileSync(path.join(__dirname, 'example_generic-span-start-end12.xliff')).toString().replace(/\n$/, '').replace(/\r$/, '')
   },
   example_nativeSpan: {
     js: require('./example_native-span.json'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_native-span.xliff')).toString().replace(/\n$/, ''),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_native-span12.xliff')).toString().replace(/\n$/, ''),
+    xliff: fs.readFileSync(path.join(__dirname, 'example_native-span.xliff')).toString().replace(/\n$/, '').replace(/\r$/, ''),
+    xliff12: fs.readFileSync(path.join(__dirname, 'example_native-span12.xliff')).toString().replace(/\n$/, '').replace(/\r$/, ''),
     js_source: require('./example_native-span_source.json'),
     js_target: require('./example_native-span_target.json')
   },
   example_nativeSpanStartEnd: {
     js: require('./example_native-span-start-end.json'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_native-span-start-end.xliff')).toString().replace(/\n$/, ''),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_native-span-start-end12.xliff')).toString().replace(/\n$/, '')
+    xliff: fs.readFileSync(path.join(__dirname, 'example_native-span-start-end.xliff')).toString().replace(/\n$/, '').replace(/\r$/, ''),
+    xliff12: fs.readFileSync(path.join(__dirname, 'example_native-span-start-end12.xliff')).toString().replace(/\n$/, '').replace(/\r$/, '')
   },
   example_invalidSegment: {
     js: require('./example_invalid-segment.json'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_invalid-segment.xliff')).toString().replace(/\n$/, ''),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_invalid-segment12.xliff')).toString().replace(/\n$/, '')
+    xliff: fs.readFileSync(path.join(__dirname, 'example_invalid-segment.xliff')).toString().replace(/\n$/, '').replace(/\r$/, ''),
+    xliff12: fs.readFileSync(path.join(__dirname, 'example_invalid-segment12.xliff')).toString().replace(/\n$/, '').replace(/\r$/, '')
   },
   // Examples showing how this could work with i18next
   example_i18next_nativeSpan: {
     js: require('./example_i18next_native-span.json'),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_i18next_native-span12.xliff')).toString().replace(/\n$/, '')
+    xliff12: fs.readFileSync(path.join(__dirname, 'example_i18next_native-span12.xliff')).toString().replace(/\n$/, '').replace(/\r$/, '')
   },
   example_i18next_unstructured_nativeSpan: {
     js: require('./example_i18next_unstructured_native-span.json'),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_i18next_unstructured_native-span12.xliff')).toString().replace(/\n$/, '')
+    xliff12: fs.readFileSync(path.join(__dirname, 'example_i18next_unstructured_native-span12.xliff')).toString().replace(/\n$/, '').replace(/\r$/, '')
   },
   example_marker: {
     js: require('./example_marker.json'),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_marker12.xliff')).toString().replace(/\n$/, '')
+    xliff12: fs.readFileSync(path.join(__dirname, 'example_marker12.xliff')).toString().replace(/\n$/, '').replace(/\r$/, '')
   },
   // angular
   example_angular: {
     js: require('./example_angular.json'),
-    xliff: fs.readFileSync(path.join(__dirname, 'example_angular.xliff')).toString().replace(/\n$/, ''),
-    xliff12: fs.readFileSync(path.join(__dirname, 'example_angular12.xliff')).toString().replace(/\n$/, ''),
-    xliff12ident: fs.readFileSync(path.join(__dirname, 'example_angular12_ident.xliff')).toString().replace(/\n$/, '')
+    xliff: fs.readFileSync(path.join(__dirname, 'example_angular.xliff')).toString().replace(/\n$/, '').replace(/\r$/, ''),
+    xliff12: fs.readFileSync(path.join(__dirname, 'example_angular12.xliff')).toString().replace(/\n$/, '').replace(/\r$/, ''),
+    xliff12ident: fs.readFileSync(path.join(__dirname, 'example_angular12_ident.xliff')).toString().replace(/\n$/, '').replace(/\r$/, '')
   }
 }
